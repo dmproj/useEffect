0df1e535a14b3083b95db61f9dad723aeca60904
SY <seyav29@gmail.com>
2023-03-04 23:20:22 -0500

    added js

--- useEff.js
+++ useEff.js
@@ -1,6 +1,4 @@
-import { EffectCallback, useEffect } from 'react'
-
-function useEffectOnce(effect: EffectCallback) {
+function useEffectOnce(effect) {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(()=>{
     if(!effect){console.log("add logic")};
